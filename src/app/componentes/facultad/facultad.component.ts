c81863b4e7f9c618982fb622e356f05772a49049
Noelia-Rivera <noelia.rivera@upeu.edu.pe>
2024-10-31 12:59:45 -0500

    facultad implemetada

--- src/app/componentes/facultad/facultad.component.ts
+++ src/app/componentes/facultad/facultad.component.ts
@@ -4,6 +4,7 @@ import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angul
 import { NgFor, NgIf } from '@angular/common';
 import { FacultadService } from '../../services/facultad.service';
 import { Facultad } from '../../models/facultad';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-facultad',
@@ -39,4 +40,92 @@ export class FacultadComponent {
       }
     );
   }
+
+  crearFacultad() {
+    const nuevafacultad = {
+      id_facultad: this.formFacultad.value.id_facultad,
+      nombre: this.formFacultad.value.nombre,
+      estado: this.formFacultad.value.estado,
+    } as any;
+    this.facultadService.crearFacultad(nuevafacultad).subscribe({
+      next: (resp) => {
+        if (resp) {
+          Swal.fire({
+            icon: 'success',
+            title: 'Libro creado',
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000
+          });
+          this.listarFacultades();
+          this.formFacultad.reset();
+        }
+      },
+      error: (err) => {
+        console.error('Error creando facultad', err);
+      }
+    });
+  }
+
+  resetFormFacultad(){
+    this.formFacultad.reset();
+  }
+
+  selectFacultad(facultad: any) {
+    this.isUpdate = true;
+    this.formFacultad.controls['id_facultad'].setValue(facultad.id_facultad);
+    this.formFacultad.controls['nombre'].setValue(facultad.nombre);
+    this.formFacultad.controls['estado'].setValue(facultad.estado);
+  }
+
+  actualizarFacultad() {
+    const facultad = {
+      id_facultad: this.formFacultad.value.id_facultad,
+      nombre: this.formFacultad.value.nombre,
+      estado: this.formFacultad.value.estado,
+    } as any;
+    this.facultadService.actualizarFacultad(facultad).subscribe({
+      next: (resp) => {
+        if (resp) {
+          Swal.fire({
+            icon: 'success',
+            title: 'Carrera actualizado',
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000
+          });
+          this.listarFacultades();
+          this.formFacultad.reset();
+        }
+      },
+      error: (err) => {
+        console.error('Error AL ACTUALIZAR', err);
+      }
+    });
+  }
+
+  eliminarFacultad(id_facultad: any){
+    Swal.fire({
+      title: "¿Estás seguro de borrar la facultad?",
+      text: "¡No serás capaz de reveritrlo!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#19e212",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Sí, borralo!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "¡Borrado!",
+          text: "El dato ha sido borrado",
+          icon: "success"
+        });
+        this.facultadService.deleteFacultad(id_facultad).subscribe(resp=>{this.listarFacultades();});
+      }
+    });
+  }
+
+
 }
